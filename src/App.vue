72afee87108d901a86ec2ef2f7b796dd40ba57d0
Geo <xr.vr.celona@gmail.com>
2024-06-28 01:10:39 +0200

    Still Fixing Cube
    Checking Errors

--- src/App.vue
+++ src/App.vue
@@ -11,9 +11,9 @@ import ThreeScene from './components/ThreeScene.vue'
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
+  </div>
   <HelloWorld msg="Vite + Vue" />
   <ThreeScene />
-  </div>
 </template>
 
 <style scoped>
